351b2d9491adc8d1b72fd807740fa0089ede2c93
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2021-11-29 21:39:33 +0700

    Update user and post relationship

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
   after_create_commit { broadcast_prepend_later_to 'posts' }
   after_update_commit { broadcast_replace_later_to 'posts' }
   after_destroy_commit { broadcast_remove_to 'posts' }
